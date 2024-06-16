1d0cd996ebb38bfb3be701bcdd23c86b92dc3d6b
twinklegoyal233 <twinklegoyal420@gmail.com>
2024-06-16 21:03:08 +0530

    hero container

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,7 +3,6 @@ export const Bio = {
     name: "Twinkle Goyal",
     roles: [
       "Frontend Developer",
-     
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
